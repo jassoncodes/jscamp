ef702f05d894a5c60003c682d4e820fe529a3d94
Jasson <jpincay93@outlook.com>
2025-11-12 20:31:41 -0500

    deploy to netlify

--- 00-html-css-js-vite/src/index.js
+++ 00-html-css-js-vite/src/index.js
@@ -55,7 +55,9 @@ const JobApp = {
  * @returns jobs
  */
 const fetchJobs = async () => {
-  return await fetch("./data/data.json").then((result) => result.json());
+  return await fetch("./public/data/data.json", { cache: "no-store" }).then(
+    (result) => result.json()
+  );
 };
 
 /**
