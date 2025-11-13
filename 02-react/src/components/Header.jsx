df5cc50ce50b52df4f6b326e83e8ae8a4889c09f
Jay <jpincay93@outlook.com>
2025-11-13 17:59:52 -0500

    working on react components

---
+++ 02-react/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const Header = () =>
+{
+    return (
+        <header>
+            <h2 class="logo">DevJobs</h2>
+
+            {/* toggle button */}
+            <button id="menu-toggle" aria-controls="mainNav" aria-expanded="false" aria-label="Abrir menú">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="icon icon-tabler icons-tabler-outline icon-tabler-menu">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M4 8l16 0" />
+                    <path d="M4 16l16 0" />
+                </svg>
+            </button>
+            <nav id="mainNav" class="menu" role="navigation">
+                <a href="./index.html">Inicio</a>
+                <a href="./search-results.html">Buscar</a>
+                <a href="./search-results.html">Empleos</a>
+                <a href="#">Salarios</a>
+            </nav>
+            <div class="header-actions">
+                <a class="menu-button" href="#">Publicar un empleo</a>
+                <a class="menu-button secondary" href="#">Iniciar sesion</a>
+            </div>
+        </header>
+    )
+}
